1a550240dd934e80c72a2b557123583388090da2
Ansar <ansarmirzayi@gmail.com>
2021-12-30 00:24:58 +0330

    declare custom axios instance and create api for user login

---
+++ src/api/Login.js
@@ -0,0 +1,6 @@
+import Axios from "./Axios";
+
+const sendPhoneNumberApi = (phone) => Axios.post(`/auth/phone`, { phone });
+const sendLoginCodeApi = (phone , code) => Axios.post(`/auth/code`, { phone , code });
+
+export { sendPhoneNumberApi, sendLoginCodeApi };
